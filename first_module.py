072fcb69454748581e6c2ca1f4ff119045d84603
Denys Naumenko <denaumenko@gmail.com>
2020-06-28 21:11:22 +0300

    Fixed background`s bag

--- first_module.py
+++ first_module.py
@@ -1,11 +1,9 @@
 from PIL import Image
-import glob
 import os
 
 file_names = []
-out_size_128 = (128,128)
-fill_color = (120,8,220)
-
+out_size_128 = (128, 128)
+fill_color = (120, 8, 220)
 
 # It recursively passes through the folders and find files
 for subdir, dirs, files in os.walk("images"):
@@ -14,12 +12,10 @@ for subdir, dirs, files in os.walk("images"):
         file_names.append(filepath)
 file_names.remove('images/.DS_Store')
 
-
 for file in file_names:
-    im = Image.open(file).convert("RGB")
-    im.thumbnail(out_size_128)
-    print(im.size,im.format)
-    im.rotate(270).save(file+".jpg", "JPEG")
-
-
-
+    im = Image.open(file).convert("RGBA")  # it had mode P after DL it from OP
+    if im.mode in ('RGBA', 'LA'):
+        background = Image.new(im.mode[:-1], im.size, fill_color)  # im.thumbnail(out_size_128)
+        background.paste(im, im.split()[-1])  # omit transparency
+        im = background
+        im.rotate(90).convert("RGB").resize(out_size_128).save(file + ".jpg", "JPEG")
